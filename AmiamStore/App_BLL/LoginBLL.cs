171fb9fa8223743859270180e8213f04073b66a6
liam ivgi <wedestv2@gmail.com>
2020-02-28 18:41:33 +0200

    Starting deal loginPage isuue

---
+++ AmiamStore/App_BLL/LoginBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using AmiamStore.App_DAL;
+using AmiamStore.Models;
+
+namespace AmiamStore.App_BLL
+{
+    public class LoginBLL
+    {
+        public List<Login> GetProductsByCata()
+        {
+            LoginDAL dal = new LoginDAL();
+            DataTable dt = dal.GetUsersData();
+
+            // converting from a DataTable to a Product Object!
+            List<Login> Users = new List<Login>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Login d = new Login();
+                d.UserName = dr["UserName"].ToString();
+                d.Password = dr["Password"].ToString();
+                Users.Add(d);
+            }
+            return Users;
+        }
+    }
+}
---
+++ AmiamStore/App_DAL/LoginDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace AmiamStore.App_DAL
+{
+    public class LoginDAL
+    {
+        public DataTable GetUsersData()
+        {
+            DBHelper dbh = new DBHelper();
+            String sql =
+               @" SELECT Users.UserName, Users.Password, Users.UserID, Users.UserType
+                   FROM  Users;";
+            DataTable dt = dbh.GetData(sql);
+            return dt;
+        }
+    }
+}
